273fe1108c3b89d390dfd2c17d5db19bbc05c985
Zaeem Raza <116446838+Zaeem-Raza@users.noreply.github.com>
2023-06-02 14:44:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function myfunction1(){
+  document.getElementById('p1').style.textDecoration='line-through';
+  document.getElementById('box1').style.background='green';
+  
+}
+function myfunction2(){
+    document.getElementById('p2').style.textDecoration='line-through';
+    document.getElementById('box2').style.background='green';
+    
+  }
+  function myfunction3(){
+    document.getElementById('p3').style.textDecoration='line-through';
+    document.getElementById('box3').style.background='green';
+    
+  }
